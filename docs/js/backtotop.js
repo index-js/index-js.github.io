3427b4fb24a75c58bdb1cbfbf910f1f8218730cd
Yanglin <i@yangl.in>
2021-10-27 22:20:11 +0800

    Trim

--- docs/js/backtotop.js
+++ docs/js/backtotop.js
@@ -1,33 +1,27 @@
 // back to top js
+let scrollTop = 0
+
 function isHidden() {
-  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
-  if (scrollTop > 0) {
-    document.querySelector(".back-to-top").className = "back-to-top";
-  } else {
-    document.querySelector(".back-to-top").className = "back-to-top hidden";
-  }
+  scrollTop = html.scrollTop || document.body.scrollTop
+  const cls = document.querySelector('.back-to-top').classList
+
+  scrollTop > 0 ? cls.remove('hidden') : cls.add('hidden')
 }
 
-const backToTop = () => {
-  let scrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop,
-    delay = 10,
-    time = 200;
-  let step = Math.ceil(scrollTop * delay / time);
+const backToTop = (ele, delay = 10, time = 200) => {
+  const step = Math.ceil(scrollTop * delay / time)
+
   let timer = setInterval(() => {
-    scrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollTop - step <= 0) {
-      document.documentElement.scrollTop = 0;
-      document.body.scrollTop = 0;
-      clearInterval(timer);
-    } else {
-      document.documentElement.scrollTop = scrollTop - step;
-      document.body.scrollTop = scrollTop - step;
+    scrollTop -= step
+    if (scrollTop <= 0) {
+      scrollTop = 0
+      clearInterval(timer)
     }
-  }, delay);
+    html.scrollTop = scrollTop
+    document.body.scrollTop = scrollTop
+  }, delay)
 }
 
 isHidden()
-document.addEventListener("scroll", isHidden, false);
-document.querySelector(".back-to-top").addEventListener("click", backToTop, false);
+document.addEventListener('scroll', isHidden, false)
+document.querySelector('.back-to-top').addEventListener('click', backToTop, false)
